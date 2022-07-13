c1e3ce5b8727eff0135e366055489ff999e07a32
Yoann Laissus <yoann.laissus@jeresiliemoncontrat.com>
2022-07-13 14:26:43 +0200

    Use yum::repo from Arakmat/puppet-yum instead of yumrepo.

--- manifests/init.pp
+++ manifests/init.pp
@@ -115,28 +115,30 @@ class epel (
 ) inherits epel::params {
   if $facts['os']['family'] == 'RedHat' and $facts['os']['name'] != 'Fedora' {
     if $epel_testing_managed {
-      yumrepo { 'epel-testing':
-        # lint:ignore:selector_inside_resource
-        mirrorlist     => $epel_testing_baseurl ? {
-          'absent' => $epel_testing_mirrorlist,
-          default  => 'absent',
-        },
-        # lint:endignore
-        baseurl        => $epel_testing_baseurl,
-        failovermethod => $epel_testing_failovermethod,
-        proxy          => $epel_testing_proxy,
-        enabled        => $epel_testing_enabled,
-        gpgcheck       => $epel_testing_gpgcheck,
-        repo_gpgcheck  => $epel_testing_repo_gpgcheck,
-        gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
-        metalink       => $epel_testing_metalink,
-        descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - Testing - \$basearch",
-        exclude        => $epel_testing_exclude,
-        includepkgs    => $epel_testing_includepkgs,
-        sslclientkey   => $epel_testing_sslclientkey,
-        sslclientcert  => $epel_testing_sslclientcert,
-        username       => $epel_testing_username,
-        password       => $epel_testing_password,
+      yum::repo { 'epel-testing':
+        yumrepo_attributes => {
+          # lint:ignore:selector_inside_resource
+          mirrorlist     => $epel_testing_baseurl ? {
+            'absent' => $epel_testing_mirrorlist,
+            default  => 'absent',
+          },
+          # lint:endignore
+          baseurl        => $epel_testing_baseurl,
+          failovermethod => $epel_testing_failovermethod,
+          proxy          => $epel_testing_proxy,
+          enabled        => $epel_testing_enabled,
+          gpgcheck       => $epel_testing_gpgcheck,
+          repo_gpgcheck  => $epel_testing_repo_gpgcheck,
+          gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
+          metalink       => $epel_testing_metalink,
+          descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - Testing - \$basearch",
+          exclude        => $epel_testing_exclude,
+          includepkgs    => $epel_testing_includepkgs,
+          sslclientkey   => $epel_testing_sslclientkey,
+          sslclientcert  => $epel_testing_sslclientcert,
+          username       => $epel_testing_username,
+          password       => $epel_testing_password,
+        }
       }
 
       if $epel_gpg_managed {
@@ -145,28 +147,30 @@ class epel (
     }
 
     if $epel_testing_debuginfo_managed {
-      yumrepo { 'epel-testing-debuginfo':
-        # lint:ignore:selector_inside_resource
-        mirrorlist     => $epel_testing_debuginfo_baseurl ? {
-          'absent' => $epel_testing_debuginfo_mirrorlist,
-          default  => 'absent',
-        },
-        # lint:endignore
-        baseurl        => $epel_testing_debuginfo_baseurl,
-        failovermethod => $epel_testing_debuginfo_failovermethod,
-        proxy          => $epel_testing_debuginfo_proxy,
-        enabled        => $epel_testing_debuginfo_enabled,
-        gpgcheck       => $epel_testing_debuginfo_gpgcheck,
-        repo_gpgcheck  => $epel_testing_debuginfo_repo_gpgcheck,
-        gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
-        metalink       => $epel_testing_debuginfo_metalink,
-        descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - Testing - \$basearch - Debug",
-        exclude        => $epel_testing_debuginfo_exclude,
-        includepkgs    => $epel_testing_debuginfo_includepkgs,
-        sslclientkey   => $epel_testing_debuginfo_sslclientkey,
-        sslclientcert  => $epel_testing_debuginfo_sslclientcert,
-        username       => $epel_testing_debuginfo_username,
-        password       => $epel_testing_debuginfo_password,
+      yum::repo { 'epel-testing-debuginfo':
+        yumrepo_attributes => {
+          # lint:ignore:selector_inside_resource
+          mirrorlist     => $epel_testing_debuginfo_baseurl ? {
+            'absent' => $epel_testing_debuginfo_mirrorlist,
+            default  => 'absent',
+          },
+          # lint:endignore
+          baseurl        => $epel_testing_debuginfo_baseurl,
+          failovermethod => $epel_testing_debuginfo_failovermethod,
+          proxy          => $epel_testing_debuginfo_proxy,
+          enabled        => $epel_testing_debuginfo_enabled,
+          gpgcheck       => $epel_testing_debuginfo_gpgcheck,
+          repo_gpgcheck  => $epel_testing_debuginfo_repo_gpgcheck,
+          gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
+          metalink       => $epel_testing_debuginfo_metalink,
+          descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - Testing - \$basearch - Debug",
+          exclude        => $epel_testing_debuginfo_exclude,
+          includepkgs    => $epel_testing_debuginfo_includepkgs,
+          sslclientkey   => $epel_testing_debuginfo_sslclientkey,
+          sslclientcert  => $epel_testing_debuginfo_sslclientcert,
+          username       => $epel_testing_debuginfo_username,
+          password       => $epel_testing_debuginfo_password,
+        }
       }
 
       if $epel_gpg_managed {
@@ -175,28 +179,30 @@ class epel (
     }
 
     if $epel_testing_source_managed {
-      yumrepo { 'epel-testing-source':
-        # lint:ignore:selector_inside_resource
-        mirrorlist     => $epel_testing_source_baseurl ? {
-          'absent' => $epel_testing_source_mirrorlist,
-          default  => 'absent',
-        },
-        # lint:endignore
-        baseurl        => $epel_testing_source_baseurl,
-        failovermethod => $epel_testing_source_failovermethod,
-        proxy          => $epel_testing_source_proxy,
-        enabled        => $epel_testing_source_enabled,
-        gpgcheck       => $epel_testing_source_gpgcheck,
-        repo_gpgcheck  => $epel_testing_source_repo_gpgcheck,
-        gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
-        metalink       => $epel_testing_source_metalink,
-        descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - Testing - \$basearch - Source",
-        exclude        => $epel_testing_source_exclude,
-        includepkgs    => $epel_testing_source_includepkgs,
-        sslclientkey   => $epel_testing_source_sslclientkey,
-        sslclientcert  => $epel_testing_source_sslclientcert,
-        username       => $epel_testing_source_username,
-        password       => $epel_testing_source_password,
+      yum::repo { 'epel-testing-source':
+        yumrepo_attributes => {
+          # lint:ignore:selector_inside_resource
+          mirrorlist     => $epel_testing_source_baseurl ? {
+            'absent' => $epel_testing_source_mirrorlist,
+            default  => 'absent',
+          },
+          # lint:endignore
+          baseurl        => $epel_testing_source_baseurl,
+          failovermethod => $epel_testing_source_failovermethod,
+          proxy          => $epel_testing_source_proxy,
+          enabled        => $epel_testing_source_enabled,
+          gpgcheck       => $epel_testing_source_gpgcheck,
+          repo_gpgcheck  => $epel_testing_source_repo_gpgcheck,
+          gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
+          metalink       => $epel_testing_source_metalink,
+          descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - Testing - \$basearch - Source",
+          exclude        => $epel_testing_source_exclude,
+          includepkgs    => $epel_testing_source_includepkgs,
+          sslclientkey   => $epel_testing_source_sslclientkey,
+          sslclientcert  => $epel_testing_source_sslclientcert,
+          username       => $epel_testing_source_username,
+          password       => $epel_testing_source_password,
+        }
       }
 
       if $epel_gpg_managed {
@@ -205,28 +211,30 @@ class epel (
     }
 
     if $epel_managed {
-      yumrepo { 'epel':
-        # lint:ignore:selector_inside_resource
-        mirrorlist     => $epel_baseurl ? {
-          'absent' => $epel_mirrorlist,
-          default  => 'absent',
-        },
-        # lint:endignore
-        baseurl        => $epel_baseurl,
-        failovermethod => $epel_failovermethod,
-        proxy          => $epel_proxy,
-        enabled        => $epel_enabled,
-        gpgcheck       => $epel_gpgcheck,
-        repo_gpgcheck  => $epel_repo_gpgcheck,
-        gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
-        metalink       => $epel_metalink,
-        descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - \$basearch",
-        exclude        => $epel_exclude,
-        includepkgs    => $epel_includepkgs,
-        sslclientkey   => $epel_sslclientkey,
-        sslclientcert  => $epel_sslclientcert,
-        username       => $epel_username,
-        password       => $epel_password,
+      yum::repo { 'epel':
+        yumrepo_attributes => {
+          # lint:ignore:selector_inside_resource
+          mirrorlist     => $epel_baseurl ? {
+            'absent' => $epel_mirrorlist,
+            default  => 'absent',
+          },
+          # lint:endignore
+          baseurl        => $epel_baseurl,
+          failovermethod => $epel_failovermethod,
+          proxy          => $epel_proxy,
+          enabled        => $epel_enabled,
+          gpgcheck       => $epel_gpgcheck,
+          repo_gpgcheck  => $epel_repo_gpgcheck,
+          gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
+          metalink       => $epel_metalink,
+          descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - \$basearch",
+          exclude        => $epel_exclude,
+          includepkgs    => $epel_includepkgs,
+          sslclientkey   => $epel_sslclientkey,
+          sslclientcert  => $epel_sslclientcert,
+          username       => $epel_username,
+          password       => $epel_password,
+        }
       }
 
       if $epel_gpg_managed {
@@ -235,28 +243,30 @@ class epel (
     }
 
     if $epel_debuginfo_managed {
-      yumrepo { 'epel-debuginfo':
-        # lint:ignore:selector_inside_resource
-        mirrorlist     => $epel_debuginfo_baseurl ? {
-          'absent' => $epel_debuginfo_mirrorlist,
-          default  => 'absent',
-        },
-        # lint:endignore
-        baseurl        => $epel_debuginfo_baseurl,
-        failovermethod => $epel_debuginfo_failovermethod,
-        proxy          => $epel_debuginfo_proxy,
-        enabled        => $epel_debuginfo_enabled,
-        gpgcheck       => $epel_debuginfo_gpgcheck,
-        repo_gpgcheck  => $epel_debuginfo_repo_gpgcheck,
-        gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
-        descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - \$basearch - Debug",
-        metalink       => $epel_debuginfo_metalink,
-        exclude        => $epel_debuginfo_exclude,
-        includepkgs    => $epel_debuginfo_includepkgs,
-        sslclientkey   => $epel_debuginfo_sslclientkey,
-        sslclientcert  => $epel_debuginfo_sslclientcert,
-        username       => $epel_debuginfo_username,
-        password       => $epel_debuginfo_password,
+      yum::repo { 'epel-debuginfo':
+        yumrepo_attributes => {
+          # lint:ignore:selector_inside_resource
+          mirrorlist     => $epel_debuginfo_baseurl ? {
+            'absent' => $epel_debuginfo_mirrorlist,
+            default  => 'absent',
+          },
+          # lint:endignore
+          baseurl        => $epel_debuginfo_baseurl,
+          failovermethod => $epel_debuginfo_failovermethod,
+          proxy          => $epel_debuginfo_proxy,
+          enabled        => $epel_debuginfo_enabled,
+          gpgcheck       => $epel_debuginfo_gpgcheck,
+          repo_gpgcheck  => $epel_debuginfo_repo_gpgcheck,
+          gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
+          descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - \$basearch - Debug",
+          metalink       => $epel_debuginfo_metalink,
+          exclude        => $epel_debuginfo_exclude,
+          includepkgs    => $epel_debuginfo_includepkgs,
+          sslclientkey   => $epel_debuginfo_sslclientkey,
+          sslclientcert  => $epel_debuginfo_sslclientcert,
+          username       => $epel_debuginfo_username,
+          password       => $epel_debuginfo_password,
+        }
       }
 
       if $epel_gpg_managed {
@@ -265,28 +275,30 @@ class epel (
     }
 
     if $epel_source_managed {
-      yumrepo { 'epel-source':
-        # lint:ignore:selector_inside_resource
-        mirrorlist     => $epel_source_baseurl ? {
-          'absent' => $epel_source_mirrorlist,
-          default  => 'absent',
-        },
-        # lint:endignore
-        baseurl        => $epel_source_baseurl,
-        failovermethod => $epel_source_failovermethod,
-        proxy          => $epel_source_proxy,
-        enabled        => $epel_source_enabled,
-        gpgcheck       => $epel_source_gpgcheck,
-        repo_gpgcheck  => $epel_source_repo_gpgcheck,
-        gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
-        descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - \$basearch - Source",
-        metalink       => $epel_source_metalink,
-        exclude        => $epel_source_exclude,
-        includepkgs    => $epel_source_includepkgs,
-        sslclientkey   => $epel_source_sslclientkey,
-        sslclientcert  => $epel_source_sslclientcert,
-        username       => $epel_source_username,
-        password       => $epel_source_password,
+      yum::repo { 'epel-source':
+        yumrepo_attributes => {
+          # lint:ignore:selector_inside_resource
+          mirrorlist     => $epel_source_baseurl ? {
+            'absent' => $epel_source_mirrorlist,
+            default  => 'absent',
+          },
+          # lint:endignore
+          baseurl        => $epel_source_baseurl,
+          failovermethod => $epel_source_failovermethod,
+          proxy          => $epel_source_proxy,
+          enabled        => $epel_source_enabled,
+          gpgcheck       => $epel_source_gpgcheck,
+          repo_gpgcheck  => $epel_source_repo_gpgcheck,
+          gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${os_maj_release}",
+          descr          => "Extra Packages for Enterprise Linux ${os_maj_release} - \$basearch - Source",
+          metalink       => $epel_source_metalink,
+          exclude        => $epel_source_exclude,
+          includepkgs    => $epel_source_includepkgs,
+          sslclientkey   => $epel_source_sslclientkey,
+          sslclientcert  => $epel_source_sslclientcert,
+          username       => $epel_source_username,
+          password       => $epel_source_password,
+        }
       }
 
       if $epel_gpg_managed {
